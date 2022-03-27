84d9917a9060b62a68a2c74791dd6fc2c91850cf
Mohamed <mohamedelyousfi87@gmail.com>
2022-03-27 23:06:33 +0200

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+iptables -A INPUT -i lo -j ACCEPT
+
+ipset -N nl hash:net
+ipset -N be hash:net
+for i in $(cat nl.zone ); do ipset -A nl $i; done
+for i in $(cat be.zone ); do ipset -A be $i; done
+for i in $(cat es.zone ); do ipset -A es $i; done
+
+iptables -I DOCKER -p udp --dport 19132 -m set --match-set nl src -j ACCEPT
+iptables -I DOCKER -p udp --dport 19132 -m set --match-set be src -j ACCEPT
+iptables -I DOCKER -p udp --dport 19132 -m set --match-set es src -j ACCEPT
