b66e5910f5d60c1d21d24385a033ec6645441bb2
Mohamed <mohamedelyousfi87@gmail.com>
2022-03-29 17:55:19 +0200

    add es ipset

--- start.sh
+++ start.sh
@@ -3,6 +3,8 @@ iptables -I DOCKER -p udp --dport 19132 -j REJECT
 
 ipset -N nl hash:net
 ipset -N be hash:net
+ipset -N es hash:net
+
 for i in $(cat nl.zone ); do ipset -A nl $i; done
 for i in $(cat be.zone ); do ipset -A be $i; done
 for i in $(cat es.zone ); do ipset -A es $i; done
